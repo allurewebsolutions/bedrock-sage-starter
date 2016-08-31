99a8417d10723a49b656ff90af8eee0755d7c06c
Mike Doubintchik <mike@allurewebsolutions.com>
2016-08-31 14:10:43 -0400

    update build script

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ git checkout master
 composer update
 git submodule update --init
 cd web/app/themes/sage-starter/
+git checkout master
 bower install
 npm install
 gulp build
+rm -rf .git .gitignore ./.gitmodules
+mv bedrock-starter/{.,}* .
+rm -rf bedrock-starter/ ./build.sh
