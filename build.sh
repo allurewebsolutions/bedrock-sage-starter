341930ccb03f7320b982abe5dbf7036b12dd6951
Mike Doubintchik <mike@allurewebsolutions.com>
2016-09-07 10:54:26 -0400

    remove erroneous bedrock-starter folder and update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 git submodule update --init
 cd bedrock-starter/
 git checkout master
-composer update
+composer install
 git submodule update --init
 cd web/app/themes/sage-starter/
 git checkout master
